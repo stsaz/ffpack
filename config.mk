528c980eeadb17b95c45d35c841bbc6cd1fecb71
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2026-06-27 17:56:13 +0300

    minor

--- config.mk
+++ config.mk
@@ -1,15 +1,24 @@
 include ../ffbase/conf.mk
 
+FPK_CF := -fpic
+CFLAGS += $(FPK_CF)
+CXXFLAGS += $(FPK_CF)
+
+FPK_LF := $(LINK_INSTALLNAME_LOADERPATH) $(LINKFLAGS_USER)
+FPK_LF += -s
+LINKFLAGS += $(FPK_LF)
+LINKXXFLAGS += $(FPK_LF) -static-libstdc++
+
 # Set utils
 CURL := curl -L
 UNTAR_ZST := tar -x --zstd -f
 UNTAR_XZ := tar xJf
 
-# Set compiler and append compiler & linker flags for Android
 SYS := $(OS)
 ifeq "$(SYS)" "android"
-	include andk.mk
-	CFLAGS += $(A_CFLAGS)
-	CXXFLAGS += $(A_CFLAGS)
-	LINKFLAGS += $(A_LINKFLAGS)
+	include ../andk.mk
+	CFLAGS := $(FPK_CF) $(A_CFLAGS)
+	CXXFLAGS := $(FPK_CF) $(A_CFLAGS)
+	LINKFLAGS := $(FPK_LF) $(A_LINKFLAGS)
+	LINKXXFLAGS := $(FPK_LF) $(A_LINKFLAGS)
 endif
